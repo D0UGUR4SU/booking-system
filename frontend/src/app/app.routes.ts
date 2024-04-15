8123800295a313f8d95ad4485b0753eb18b285b4
D4GUR4SU <douglas_search@hotmail.com>
2024-04-14 23:13:49 -0300

    Creating routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {AppComponent} from "./app.component";
+import {LoginComponent} from "./components/login/login.component";
+import {LocationsComponent} from "./components/locations/locations.component";
+import {VenueDetailsComponent} from "./components/venue-details/venue-details.component";
+import {BookingManagementComponent} from "./components/booking-management/booking-management.component";
+import {NgModule} from "@angular/core";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: AppComponent},
+  {path: '/auth/login', component: LoginComponent},
+  {path: '/locations', component: LocationsComponent},
+  {path: '/venue-details', component: VenueDetailsComponent},
+  {path: '/booking-management', component: BookingManagementComponent},
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutes {}
