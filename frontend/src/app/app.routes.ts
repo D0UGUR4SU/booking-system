adf9871f3f1ae2e23e5e445b936de79d1d3351c6
D4GUR4SU <douglas_search@hotmail.com>
2024-04-15 03:08:46 -0300

    Fixing login page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from "@angular/core";
+import {LoginComponent} from "./components/login/login.component";
 import {AppComponent} from "./app.component";
 
 export const routes: Routes = [
-  {path: '/', component: AppComponent},
+  {path: '', component: AppComponent},
+  {path: 'login', component: LoginComponent},
   { path: '**', redirectTo:'home' }
 ];
 
